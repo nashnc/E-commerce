d7e6b6c40295bdbceb4419b62ca7cef91b22ae9a
nashnc <nash.nc@gmail.com>
2025-06-14 00:12:07 +0530

    day 2 added making conumer side with react  commit

--- a.md
+++ a.md
@@ -1,4 +1,4 @@
-git commit -m "first commit"
+git commit -m "day 2 added making conumer side with react  commit"
 git branch -M main
 git remote add origin git@github.com:nashnc/E-commerce.git
 git push -u origin main
--- consumerSide/src/components/Header/Header.jsx
+++ consumerSide/src/components/Header/Header.jsx
@@ -1,7 +1,24 @@
 import { Link } from "react-router-dom";
 import Search from "../Search/Search";
+import Badge from "@mui/material/Badge";
+
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import { IoCartOutline } from "react-icons/io5";
+import { IoGitCompareOutline } from "react-icons/io5";
+import { IoHeartOutline } from "react-icons/io5";
+import Tooltip from "@mui/material/Tooltip";
 
 const Header = () => {
+  const StyledBadge = styled(Badge)(({ theme }) => ({
+    "& .MuiBadge-badge": {
+      right: -3,
+      top: 13,
+      border: `2px solid ${theme.palette.background.paper}`,
+      padding: "0 4px",
+    },
+  }));
+
   return (
     <>
       <header>
@@ -45,7 +62,51 @@ const Header = () => {
             <div className="col2 w-[45%]">
               <Search />
             </div>
-            <div className="col3 w-[30%]"></div>
+            <div className="col3 w-[30%] pl-7">
+              <ul className="flex w-full items-center justify-end gap-3 text-[16px]">
+                <li className="list-none">
+                  <Link to="/login" className="link font-[500] transition">
+                    Login
+                  </Link>{" "}
+                  /{"  "}
+                  <Link to="/register" className="link font-[500] transition">
+                    Signup
+                  </Link>
+                </li>
+
+                <li>
+                  {" "}
+                  <Tooltip title="Compare" placement="bottom">
+                    <IconButton aria-label="compare">
+                      <StyledBadge badgeContent={4} color="secondary">
+                        <IoGitCompareOutline />
+                      </StyledBadge>
+                    </IconButton>
+                  </Tooltip>
+                </li>
+
+                <li>
+                  {" "}
+                  <Tooltip title="Cart" placement="bottom">
+                    <IconButton aria-label="cart">
+                      <StyledBadge badgeContent={4} color="secondary">
+                        <IoCartOutline />
+                      </StyledBadge>
+                    </IconButton>
+                  </Tooltip>
+                </li>
+                <li>
+                  {" "}
+                  <Tooltip title="Favorite" placement="bottom">
+                    <IconButton aria-label="favorite">
+                      <StyledBadge badgeContent={4} color="secondary">
+                        <IoHeartOutline />
+                      </StyledBadge>
+                    </IconButton>
+                  </Tooltip>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </header>
