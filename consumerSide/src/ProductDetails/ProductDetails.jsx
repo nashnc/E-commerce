1c07bb9b61c7fdf4747e26d6bab4235784917d1a
nashnc <nash.nc@gmail.com>
2025-07-13 01:29:16 +0530

    day 13 added related products with product slider, changes made with UI in productContentContainer

--- consumerSide/src/ProductDetails/ProductDetails.jsx
+++ consumerSide/src/ProductDetails/ProductDetails.jsx
@@ -3,6 +3,7 @@ import AdditionalDetailsOfProducts from "../components/ProductDetailsSub/Additio
 import ProductContent from "../components/ProductDetailsSub/ProductContent/ProductContent";
 import ProductZoom from "../components/ProductDetailsSub/ProductZoom/ProductZoom";
 import React from "react";
+import ProductSlider from "../components/Sliders/ProductSlider/ProductSlider";
 
 const ProductDetails = () => {
      const crumbs = [
@@ -24,12 +25,19 @@ const ProductDetails = () => {
                          <div className="productZoomContainer h-[500px] w-[40%] overflow-hidden">
                               <ProductZoom />
                          </div>
-                         <div className="productContentContainer w-[60%]">
+                         <div className="productContentContainer pr-10 pl-10 w-[60%]">
                               <ProductContent />
                          </div>
                     </div>
                     <div className="container pt-10">
                          <AdditionalDetailsOfProducts />
+                    </div>
+                    <div className="container mt-5">
+                         <h2 className="text-[20px] pb-0 font-[600]">
+                              Related Products
+                         </h2>
+
+                         <ProductSlider items={6} />
                     </div>
                </section>
           </>
