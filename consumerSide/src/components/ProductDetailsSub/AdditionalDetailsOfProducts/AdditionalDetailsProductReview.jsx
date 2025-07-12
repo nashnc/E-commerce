3f23c94479cc4024871ad74cd578f04ae91ce0eb
nashnc <nash.nc@gmail.com>
2025-07-12 22:25:10 +0530

    day 12 new  added additional details product review component

---
+++ consumerSide/src/components/ProductDetailsSub/AdditionalDetailsOfProducts/AdditionalDetailsProductReview.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const AdditionalDetailsProductReview = () => {
+     return (
+          <>
+               <div className="productsReviewContainer w-full">
+                    <h2 className="text-[18px]">
+                         Customer questions & answers
+                    </h2>
+                    <div className="scroll w-full max-f-[300px]"></div>
+               </div>
+          </>
+     );
+};
+
+export default AdditionalDetailsProductReview;
