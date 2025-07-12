9c6fc660efe5fe634d73687393be1df1e9bd57cb
nashnc <nash.nc@gmail.com>
2025-07-13 01:41:39 +0530

    day 13 created this component  for    writing review in the products details page in the review area,  also added | text field with MUI |product rating | Submit button

---
+++ consumerSide/src/components/ProductDetailsSub/AdditionalDetailsOfProducts/AdditionalDetailsOfProductsReviewTextField.jsx
@@ -0,0 +1,32 @@
+import ProductItemRating from "../../Sliders/ProductSlider/ProductItemRating";
+import { Button } from "@mui/material";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import React from "react";
+
+const AdditionalDetailsOfProductsReviewTextField = () => {
+     return (
+          <>
+               <div>
+                    <form action="" className="w-full mt-5">
+                         <TextField
+                              id="outlined-multiline-static"
+                              label="Write a review ..."
+                              multiline
+                              rows={5}
+                              className="w-full"
+                         />
+                    </form>
+                    <br />
+                    <br />
+
+                    <ProductItemRating readAble="false" />
+
+                    <div className="flex items-center mt-5">
+                         <Button className="btn-orange">Submit Review</Button>
+                    </div>
+               </div>
+          </>
+     );
+};
+export default AdditionalDetailsOfProductsReviewTextField;
