56de2789601c8b30ec00289f13a2be8c3a60671d
nashnc <nash.nc@gmail.com>
2025-07-12 22:27:23 +0530

    day 12 Added additional product details  component  with tailwind tables

---
+++ consumerSide/src/components/ProductDetailsSub/AdditionalDetailsOfProducts/AdditionalDetailsOfProducts.jsx
@@ -0,0 +1,42 @@
+import "./AdditionalDetailsOfProducts.css";
+import AdditionalDetailsProductDescription from "./AdditionalDetailsProductDescription";
+import AdditionalDetailsProductDetails from "./AdditionalDetailsProductDetails";
+import AdditionalDetailsProductReview from "./AdditionalDetailsProductReview";
+import React, { useState } from "react";
+
+const AdditionalDetailsOfProducts = () => {
+     const [activeTab, setActiveTab] = useState(0);
+
+     return (
+          <>
+               <div className="flex items-center gap-10 mb-5 ">
+                    <h3
+                         onClick={() => setActiveTab(0)}
+                         className={`link text-[18px] cursor-pointer font-[600] ${activeTab === 0 ? "text-primary" : ""}`}
+                    >
+                         Description
+                    </h3>
+                    <h3
+                         onClick={() => setActiveTab(1)}
+                         className={`link text-[18px] cursor-pointer font-[600] ${activeTab === 1 ? "text-primary" : ""}`}
+                    >
+                         Product Details
+                    </h3>
+                    <h3
+                         onClick={() => setActiveTab(2)}
+                         className={`link text-[18px] cursor-pointer font-[600] ${activeTab === 2 ? "text-primary" : ""}`}
+                    >
+                         Reviews (5)
+                    </h3>
+               </div>
+               <div className="shadow-md w-full p-5  rounded-md">
+                    {activeTab === 0 && <AdditionalDetailsProductDescription />}
+
+                    {activeTab === 1 && <AdditionalDetailsProductDetails />}
+                    {activeTab === 2 && <AdditionalDetailsProductReview />}
+               </div>
+          </>
+     );
+};
+
+export default AdditionalDetailsOfProducts;
---
+++ consumerSide/src/components/ProductDetailsSub/AdditionalDetailsOfProducts/AdditionalDetailsProductDetails.jsx
@@ -0,0 +1,155 @@
+import React from "react";
+
+const AdditionalDetailsProductDetails = () => {
+     return (
+          <>
+               <div className="productDetails">
+                    <div className="relative overflow-x-auto">
+                         <table className="w-full text-sm text-left rtl:text-right font- text-gray-500">
+                              <tbody>
+                                   <tr className="bg-white border-b border-gray-200">
+                                        <th
+                                             scope="row"
+                                             className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                                        >
+                                             Stand Up
+                                        </th>
+                                        <td className="px-6 py-4">
+                                             35″L x 24″W x 37-45″H(front to back
+                                             wheel)
+                                        </td>
+                                   </tr>
+                                   <tr className="bg-white border-b border-gray-200">
+                                        <th
+                                             scope="row"
+                                             className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                                        >
+                                             Folded (w/o wheels)
+                                        </th>
+                                        <td className="px-6 py-4">
+                                             32.5″L x 18.5″W x 16.5″H
+                                        </td>
+                                   </tr>
+                                   <tr className="bg-white border-b border-gray-200">
+                                        <th
+                                             scope="row"
+                                             className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                                        >
+                                             Folded (w/ wheels)
+                                        </th>
+                                        <td className="px-6 py-4">
+                                             32.5″L x 24″W x 18.5″H
+                                        </td>
+                                   </tr>
+                                   <tr className="bg-white border-b border-gray-200">
+                                        <th
+                                             scope="row"
+                                             className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                                        >
+                                             Door Pass Through
+                                        </th>
+                                        <td className="px-6 py-4">24</td>
+                                   </tr>
+                                   <tr className="bg-white border-b border-gray-200">
+                                        <th
+                                             scope="row"
+                                             className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                                        >
+                                             Frame
+                                        </th>
+                                        <td className="px-6 py-4">Aluminum</td>
+                                   </tr>
+                                   <tr className="bg-white border-b border-gray-200">
+                                        <th
+                                             scope="row"
+                                             className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                                        >
+                                             Weight (w/o wheels)
+                                        </th>
+                                        <td className="px-6 py-4">20 LBS</td>
+                                   </tr>
+                                   <tr className="bg-white border-b border-gray-200">
+                                        <th
+                                             scope="row"
+                                             className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                                        >
+                                             Weight Capacity
+                                        </th>
+                                        <td className="px-6 py-4">60 LBS</td>
+                                   </tr>
+                                   <tr className="bg-white border-b border-gray-200">
+                                        <th
+                                             scope="row"
+                                             className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                                        >
+                                             Width
+                                        </th>
+                                        <td className="px-6 py-4">24″</td>
+                                   </tr>
+                                   <tr className="bg-white border-b border-gray-200">
+                                        <th
+                                             scope="row"
+                                             className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                                        >
+                                             Handle height (ground to handle)
+                                        </th>
+                                        <td className="px-6 py-4">37-45″</td>
+                                   </tr>
+                                   <tr className="bg-white border-b border-gray-200">
+                                        <th
+                                             scope="row"
+                                             className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                                        >
+                                             Wheels
+                                        </th>
+                                        <td className="px-6 py-4">
+                                             12″ air / wide track slick tread
+                                        </td>
+                                   </tr>
+                                   <tr className="bg-white border-b border-gray-200">
+                                        <th
+                                             scope="row"
+                                             className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                                        >
+                                             Seat back height
+                                        </th>
+                                        <td className="px-6 py-4">21.5″</td>
+                                   </tr>
+                                   <tr className="bg-white border-b border-gray-200">
+                                        <th
+                                             scope="row"
+                                             className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                                        >
+                                             Head room (inside canopy)
+                                        </th>
+                                        <td className="px-6 py-4">25″</td>
+                                   </tr>
+                                   <tr className="bg-white border-b border-gray-200">
+                                        <th
+                                             scope="row"
+                                             className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                                        >
+                                             Color
+                                        </th>
+                                        <td className="px-6 py-4">
+                                             Black, Blue, Red, White
+                                        </td>
+                                   </tr>
+                                   <tr className="bg-white border-b border-gray-200">
+                                        <th
+                                             scope="row"
+                                             className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                                        >
+                                             Size
+                                        </th>
+                                        <td className="px-6 py-4">M, S</td>
+                                   </tr>
+                              </tbody>
+                         </table>
+                    </div>
+               </div>
+          </>
+     );
+};
+
+export default AdditionalDetailsProductDetails;
