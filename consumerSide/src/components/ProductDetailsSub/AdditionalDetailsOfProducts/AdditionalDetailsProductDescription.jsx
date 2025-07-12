1c730a0740924c25c53ead0b944a3ec4d6a277aa
nashnc <nash.nc@gmail.com>
2025-07-13 01:39:09 +0530

    day 13  created this for better code readability and to avoid messy work

--- consumerSide/src/components/ProductDetailsSub/AdditionalDetailsOfProducts/AdditionalDetailsProductDescription.jsx
+++ consumerSide/src/components/ProductDetailsSub/AdditionalDetailsOfProducts/AdditionalDetailsProductDescription.jsx
@@ -3,37 +3,42 @@ import React from "react";
 const AdditionalDetailsProductDescription = () => {
      return (
           <>
-               <div className=" productDescription">
-                    <p>
-                         The best is yet to come! Give your walls a voice with a
-                         framed poster. This aesthethic, optimistic poster will
-                         look great in your desk or in an open-space office.
-                         Painted wooden frame with passe-partout for more depth
-                    </p>
-                    <h3>Lightweight Design</h3>
-                    <p>
-                         Designed with a super light geometric case, the Versa
-                         family watches are slim, casual and comfortable enough
-                         to wear all day and night. Switch up your look with
-                         classic, leather, metal and woven accessory bands. Ut
-                         elit tellus, luctus nec ullamcorper mattis, pulvinar
-                         dapibus leo.
-                    </p>
-                    <h3>Free Shipping & Return</h3>
-                    <p>
-                         We offer free shipping for products on orders above 50$
-                         and offer free delivery for all orders in US.
-                    </p>
-                    <h3>Money Back Guarantee</h3>
-                    <p>
-                         We guarantee our products and you could get back all of
-                         your money anytime you want in 30 days.
-                    </p>
-                    <h3>Online Support</h3>
-                    <p>
-                         You will get 24 hour support with this purchase product
-                         and you can return it within 30 days for an exchange.
-                    </p>{" "}
+               <div className="shadow-md w-full p-5  rounded-md">
+                    <div className=" productDescription">
+                         <p>
+                              The best is yet to come! Give your walls a voice
+                              with a framed poster. This aesthethic, optimistic
+                              poster will look great in your desk or in an
+                              open-space office. Painted wooden frame with
+                              passe-partout for more depth
+                         </p>
+                         <h3>Lightweight Design</h3>
+                         <p>
+                              Designed with a super light geometric case, the
+                              Versa family watches are slim, casual and
+                              comfortable enough to wear all day and night.
+                              Switch up your look with classic, leather, metal
+                              and woven accessory bands. Ut elit tellus, luctus
+                              nec ullamcorper mattis, pulvinar dapibus leo.
+                         </p>
+                         <h3>Free Shipping & Return</h3>
+                         <p>
+                              We offer free shipping for products on orders
+                              above 50$ and offer free delivery for all orders
+                              in US.
+                         </p>
+                         <h3>Money Back Guarantee</h3>
+                         <p>
+                              We guarantee our products and you could get back
+                              all of your money anytime you want in 30 days.
+                         </p>
+                         <h3>Online Support</h3>
+                         <p>
+                              You will get 24 hour support with this purchase
+                              product and you can return it within 30 days for
+                              an exchange.
+                         </p>{" "}
+                    </div>
                </div>
           </>
      );
