305622bbfd4ae3480a16918ec5433e7aadc48219
nashnc <nash.nc@gmail.com>
2025-07-13 14:59:32 +0530

    day 13 used useContext to add a popup modal for products when the pop up button is clicked the component is imported here and the props are passed

--- consumerSide/src/App.jsx
+++ consumerSide/src/App.jsx
@@ -1,33 +1,46 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import ProductDetails from "./ProductDetails/ProductDetails";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import ShowProductDetailsModal from "./components/ShowProductDetailsModal/ShowProductDetailsModal";
 import Home from "./pages/Home/Home";
 import ProductListing from "./pages/ProductListing/ProductListing";
-import Footer from "./components/Footer/Footer";
-import ProductDetails from "./ProductDetails/ProductDetails";
+import { createContext, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const MyContext = createContext();
+
 function App() {
-     return (
-          <>
-               <BrowserRouter>
-                    <Header />
-                    <Routes>
-                         <Route path={"/"} exact={true} element={<Home />} />
-                         <Route
-                              path={"/items"}
-                              exact={true}
-                              element={<ProductListing />}
-                         />
-                         <Route
-                              path={"/seeitems/:id"}
-                              exact={true}
-                              element={<ProductDetails />}
-                         />
-                    </Routes>
-                    <Footer />
-               </BrowserRouter>
-          </>
-     );
+  const [openProductDetailsModal, setOpenProductDetailsModal] = useState(false);
+
+  const handleCloseProductDetailsModal = () => {
+    setOpenProductDetailsModal(false);
+  };
+  const values = { setOpenProductDetailsModal };
+  return (
+    <>
+      <BrowserRouter>
+        <MyContext.Provider value={values}>
+          <Header />
+          <Routes>
+            <Route path={"/"} exact={true} element={<Home />} />
+            <Route path={"/items"} exact={true} element={<ProductListing />} />
+            <Route
+              path={"/seeitems/:id"}
+              exact={true}
+              element={<ProductDetails />}
+            />
+          </Routes>
+          <Footer />
+        </MyContext.Provider>
+      </BrowserRouter>
+      <ShowProductDetailsModal
+        open={openProductDetailsModal}
+        close={handleCloseProductDetailsModal}
+      />
+    </>
+  );
 }
 
 export default App;
- 
+export { MyContext };
