0cf5cc3f536845f4f244f52ee5f89b1a582ab10f
nashnc <nash.nc@gmail.com>
2025-07-14 11:25:40 +0530

    day 14 moved the bottom section to  a different  component

--- consumerSide/src/components/CartSlide/CartSlide.jsx
+++ consumerSide/src/components/CartSlide/CartSlide.jsx
@@ -4,6 +4,7 @@ import "./CartSlide.css";
 import Drawer from "@mui/material/Drawer";
 import React from "react";
 import { IoCloseSharp } from "react-icons/io5";
+import CartBottomInfo from "./CartBottomInfo";
 
 const CartSlide = (props) => {
   return (
@@ -25,28 +26,8 @@ const CartSlide = (props) => {
           <CartItem />
         </div>
 
-        <div className="bottomSection absolute bottom-[10px] w-full p-5">
-          <div className="bottomInfo border-[rgba(0,0,0,0.1 )] flex w-full flex-col items-center justify-between border-t px-4 py-3">
-            <div className="flex w-full items-center justify-between">
-              <p className="text-[14px] font-[600]">2 item </p>
-              <p className="font-bold text-primary">$106.00</p>
-            </div>
-            <div className="flex w-full items-center justify-between">
-              <p className="text-[14px] font-[600]">Shipping</p>
-              <p className="font-bold text-primary">$16.00</p>
-            </div>
-          </div>{" "}
-          <div className="bottomInfo border-[rgba(0,0,0,0.1 )] flex w-full flex-col items-center justify-between border-t px-4 py-3">
-            <div className="flex w-full items-center justify-between">
-              <p className="text-[14px] font-[600]">Total (tax excl.) </p>
-              <p className="font-bold text-primary">$136.00</p>
-            </div>
-          </div>{" "}
-          <button></button>
-          <div className="flex w-full items-center justify-between gap-5">
-            <Button className="btn-orange btn-lg w-[50%]">View Cart</Button>
-            <Button className="btn-orange btn-lg w-[50%]">CheckOut</Button>
-          </div>
+        <div className="">
+          <CartBottomInfo />
         </div>
       </Drawer>
     </>
