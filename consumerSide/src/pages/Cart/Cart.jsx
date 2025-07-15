71d8280820b8ec2f29e46aad825498e2dcb9aed0
nashnc <nash.nc@gmail.com>
2025-07-15 14:41:21 +0530

    day 15 - created a cart component with left and right sections

---
+++ consumerSide/src/pages/Cart/Cart.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import CartLeftSection from "../../components/Cart/CartLeftSection";
+import "./Cart.css";
+import CartRightSection from "../../components/Cart/CartRightSection";
+const Cart = () => {
+  return (
+    <div className="section py-10">
+      <div className="left container">
+        <div className="flex w-[80%] max-w-[80%] gap-5">
+          <div className="leftPart w-[70%]">
+            <CartLeftSection />
+          </div>{" "}
+          <div className="rightPart w-[30%]">
+            <CartRightSection />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
