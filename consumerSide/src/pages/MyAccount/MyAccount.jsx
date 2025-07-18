1c69af8439ecbd0ab2f47cae3f9a9efbe9d1e41b
nashnc <nash.nc@gmail.com>
2025-07-18 19:53:38 +0530

    day 18: added MyAccount page component; imported subcomponents <MyAccountLeft /> and <MyAccountRight /> for better readability

---
+++ consumerSide/src/pages/MyAccount/MyAccount.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import MyAccountLeft from "../../components/MyAccount/MyAccountLeft";
+import MyAccountRight from "../../components/MyAccount/MyAccountRight";
+
+const MyAccount = () => {
+  return (
+    <>
+      <section className="w-full py-10">
+        <div className="container flex gap-5">
+          <div className="col1 w-[20%]">
+            <MyAccountLeft />
+          </div>
+          <div className="col2 w-[50%]">
+            <MyAccountRight />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default MyAccount;
