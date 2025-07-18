e08917c2ebfc0d8669e9ed67e7748dad372816c5
nashnc <nash.nc@gmail.com>
2025-07-18 20:27:14 +0530

    day 18: removed unnecessary characters {" "}

--- consumerSide/src/components/CartSlide/CartBottomInfo.jsx
+++ consumerSide/src/components/CartSlide/CartBottomInfo.jsx
@@ -15,13 +15,13 @@ const CartBottomInfo = () => {
             <p className="text-[14px] font-[600]">Shipping</p>
             <p className="font-bold text-primary">$16.00</p>
           </div>
-        </div>{" "}
+        </div>
         <div className="bottomInfo border-[rgba(0,0,0,0.1 )] flex w-full flex-col items-center justify-between border-t px-4 py-3">
           <div className="flex w-full items-center justify-between">
             <p className="text-[14px] font-[600]">Total (tax excl.) </p>
             <p className="font-bold text-primary">$136.00</p>
           </div>
-        </div>{" "}
+        </div>
         <div className="flex w-full items-center justify-between gap-5">
           <Button
             component={Link}
@@ -33,7 +33,7 @@ const CartBottomInfo = () => {
           <Button
             component={Link}
             to="/checkout"
-            className="btn-orange btn-lg w-[50%]"
+            className="btn-orange btn-border btn-lg w-[50%]"
           >
             CheckOut
           </Button>
