34046bc20cdfe8de402e9bc3e91d77d53a263072
nashnc <nash.nc@gmail.com>
2025-07-19 23:29:22 +0530

    day 19: updated UI with heading and description to display number of items in cart

--- consumerSide/src/components/Cart/CartLeftSection.jsx
+++ consumerSide/src/components/Cart/CartLeftSection.jsx
@@ -4,11 +4,16 @@ import CartLeftSectionCartItem from "./CartLeftSectionCartItem";
 const CartLeftSection = () => {
   return (
     <>
-    <div className="py-2 px-3 border-b border-[rgba(0,0,0,0.2)]"></div>
       <div className="rounded-md border-b border-[rgba(0,0,0,0.2)] bg-white pb-5 shadow-md">
-        <CartLeftSectionCartItem />
+        <div className="border-b border-[rgba(0,0,0,0.2)] px-3 py-2">
+          <h2>Your Cart</h2>
+          <p className="mb-0 mt-0">
+            There are <span className="font-bold text-primary"> 2 </span>
+            products in your cart
+          </p>
+          <CartLeftSectionCartItem />
+        </div>
       </div>
-  
     </>
   );
 };
