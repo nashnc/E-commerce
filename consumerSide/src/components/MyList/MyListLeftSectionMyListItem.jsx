f1a68ef6f152c96f2c056fb9caee8b101146bcec
nashnc <nash.nc@gmail.com>
2025-07-19 23:20:46 +0530

    day 19: created MyListLeftSectionMyListItem component to display brand name, item name, rating, price, discount, and add-to-cart button

---
+++ consumerSide/src/components/MyList/MyListLeftSectionMyListItem.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { IoCloseSharp } from "react-icons/io5";
+import ProductItemRating from "../Sliders/ProductSlider/ProductItemRating";
+import { Button } from "@mui/material";
+
+const MyListLeftSectionMyListItem = () => {
+  const image = [
+    "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/33399100/2025/4/5/6478af3f-ce1a-469e-ae8c-bb88adac1f241743850680594-House-of-Pataudi-Chevron-Embroidered-Straight-Kurta-22417438-1.jpg",
+  ];
+  return (
+    <>
+      <div className="MyListItem flex w-full items-center gap-4 p-3">
+        <div className="img group w-[15%] overflow-hidden rounded-md">
+          <img
+            src={image}
+            alt=""
+            component={Link}
+            to="/product/165"
+            className="transition-all group-hover:scale-105"
+          />
+        </div>
+        <div className="info relative w-[85%]">
+          <IoCloseSharp className="absolute right-[0px] top-[0px] cursor-pointer text-[22px]" />
+          <p className="text-[13px]">House of Pataudi </p>
+          <h3 component={Link} to="/product/165" className="link text-[16px]">
+            Chevron Embroidered Straight Kurta
+          </h3>
+          <ProductItemRating score="4" />
+          <div className="mb-2 mt-2 flex items-center gap-4">
+            <span className="newPrice text-[14px] font-[600] text-[#ff5252]">
+              $58.00
+            </span>
+            <span className="oldPrice text-[14px] font-[400] text-[rgba(0,0,0,0.5)] line-through">
+              $58.00
+            </span>
+            <span className="newPrice text-[14px] text-[#ff5252]">55% OFF</span>
+          </div>
+          <Button className="btn-orange btn-sm">Add to cart</Button>{" "}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MyListLeftSectionMyListItem;
