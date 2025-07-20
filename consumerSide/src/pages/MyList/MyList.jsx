c00a102bad3ab395cf02cfb80122bd373ade8980
nashnc <nash.nc@gmail.com>
2025-07-20 13:48:00 +0530

    Day 20: Modified Tailwind classes for better UI

--- consumerSide/src/pages/MyList/MyList.jsx
+++ consumerSide/src/pages/MyList/MyList.jsx
@@ -10,7 +10,7 @@ const MyList = () => {
           <div className="col1 w-[20%]">
             <MyAccountLeft />
           </div>
-          <div className="col2 w-[50%]">
+          <div className="col2 w-[80%]">
             <MyListLeftSection />
           </div>
         </div>
