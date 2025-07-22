5893ae0a54ca1d49dd794f708b5246e3e19387f5
nashnc <nash.nc@gmail.com>
2025-07-22 14:54:28 +0530

    Day 22: Imported <DashBoardWelcome />, <DashBoardBox />, and <DashBoardTable /> into dashboard.jsx

--- adminPanel/src/Pages/DashBoard/Dashboard.jsx
+++ adminPanel/src/Pages/DashBoard/Dashboard.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import DashBoardBox from "../../Components/DashBoardBox/DashBoardBox";
+import DashBoardWelcome from "../../Components/DashBoardBox/DashBoardWelcome";
+import DashBoardTable from "../../Components/DashBoardBox/DashBoardTable";
+import './DashBoard.css'
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <DashBoardWelcome />
+
+      <DashBoardBox />
+      <DashBoardTable />
+    </>
+  );
 };
 
 export default Dashboard;
