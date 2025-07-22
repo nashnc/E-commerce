5877c2d2da2361ff4d64a9bd9a7597bc684d377e
nashnc <nash.nc@gmail.com>
2025-07-22 14:59:29 +0530

    Day 22: Created a badge for showing order status

---
+++ adminPanel/src/Components/Badge/Badge.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Badge = (props) => {
+  return (
+    <>
+      <span
+        className={`flex items-center justify-center rounded-full px-2 py-1 text-[12px] capitalize ${props.status === "pending" && "bg-primary text-white"} ${props.status === "delivered" && "bg-green-700 text-white"} ${props.status === "confirm" && "bg-green-500 text-white"} `}
+      >
+        {props.status}
+      </span>
+    </>
+  );
+};
+
+export default Badge;
