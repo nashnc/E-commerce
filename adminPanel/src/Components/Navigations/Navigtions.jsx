17d951e729b0bb04dfbd503518dc5687f91d0a74
nashnc <nash.nc@gmail.com>
2025-07-22 14:56:32 +0530

    Day 22: Imported <Dashboard />

--- adminPanel/src/Components/Navigations/Navigtions.jsx
+++ adminPanel/src/Components/Navigations/Navigtions.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../Header/Header";
 import SideBar from "../SideBar/Sidebar";
+import Dashboard from "../../Pages/DashBoard/Dashboard";
 const Navigtions = () => {
   return (
     <>
@@ -9,6 +10,9 @@ const Navigtions = () => {
         <div className="contentMain flex">
           <div className="sideWrapper w-[18%]">
             <SideBar />
+          </div>
+          <div className="contentRight w-[82%] px-5 py-4">
+            <Dashboard />
           </div>
         </div>
       </section>
