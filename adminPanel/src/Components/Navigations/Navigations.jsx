15f89c217e4ff5c5a78a2e12050a240784c6c52d
nashnc <nash.nc@gmail.com>
2025-07-23 14:33:22 +0530

    feat(navigation): Day 23 of e-commerce self project - add vertical nav with context-based layout control
    Renamed Navigtions to Navigations. Implemented logic to expand or shrink
    page layout based on context. Added vertical navigation for im

---
+++ adminPanel/src/Components/Navigations/Navigations.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+import Header from "../Header/Header";
+import SideBar from "../SideBar/Sidebar";
+import Dashboard from "../../Pages/DashBoard/Dashboard";
+import { MyContext } from "../../App"; // Update this line
+const Navigations = () => {
+  const context = useContext(MyContext);
+  return (
+    <>
+      <section className="main">
+        <Header />
+        <div className="contentMain flex">
+          <div
+            className={`sideWrapper transition-all duration-[1s] ${context.sideBarIsOpen ? "w-[18%]" : "w-[4%] overflow-hidden"}`}
+          >
+            <SideBar />
+          </div>
+          <div
+            className={`contentRight transition-all duration-[1s] ${context.sideBarIsOpen ? "w-[82%]" : "w-[90%]"} px-5 py-4`}
+          >
+            <Dashboard />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Navigations;
