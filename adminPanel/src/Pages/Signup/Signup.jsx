fc2fa5950579ad93da34db1c3d7b826aa0e7f94c
nashnc <nash.nc@gmail.com>
2025-07-24 19:42:52 +0530

    feat: Day 24 e-commerce self-project - create signup page and integrate <LoginSignupNavs /> component for navigation

---
+++ adminPanel/src/Pages/Signup/Signup.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginSignupNavs from "../../Components/LoginSiginUp/LoginSignupNavs";
+
+const Signup = () => {
+  return (
+    <>
+      <LoginSignupNavs />
+    </>
+  );
+};
+
+export default Signup;
