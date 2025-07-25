43580707e1e532e22cdc53beb195ad00fff39efd
nashnc <nash.nc@gmail.com>
2025-07-25 20:52:34 +0530

    feat: Day 25 e-commerce self-project - update Signup.jsx with background image, <LoginSignupNavs />, and modular components for signup flow

--- adminPanel/src/Pages/Signup/Signup.jsx
+++ adminPanel/src/Pages/Signup/Signup.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import LoginSignupNavs from "../../Components/LoginSiginUp/LoginSignupNavs";
+import LoginBox from "../../Components/LoginSiginUp/LoginBox";
+import LoginDivider from "../../Components/LoginSiginUp/LoginDivider";
+import LoginForm from "../../Components/LoginSiginUp/LoginForm";
 
 const Signup = () => {
   return (
     <>
+      <img
+        src="/bkgnd.jpg"
+        className="absolute left-0 top-0 -z-10 h-screen w-full object-cover opacity-25"
+        alt=""
+      />
       <LoginSignupNavs />
+      <LoginBox show="signup" />
+      <LoginDivider text="Signup" />
+      <LoginForm show="signup" />
     </>
   );
 };
