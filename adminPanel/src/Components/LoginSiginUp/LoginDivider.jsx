08abc5d23dbd9cb09c8b04d77c086e298cfe512e
nashnc <nash.nc@gmail.com>
2025-07-25 20:54:27 +0530

    refactor: Day 25 e-commerce self-project - update components to handle logic conditionally based on props for login and signup reusability

--- adminPanel/src/Components/LoginSiginUp/LoginDivider.jsx
+++ adminPanel/src/Components/LoginSiginUp/LoginDivider.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const LoginDivider = () => {
+const LoginDivider = (props) => {
   return (
     <>
       <div className="flex w-full items-center justify-center gap-3">
-        <hr className="flex h-[2px] w-[100px] items-end bg-slate-500" />
-        <p>or, Signin in with e-mail </p>
-        <hr className="flex h-[2px] w-[100px] items-end bg-slate-500" />
+        <hr className="flex h-[2px] w-[100px] items-end bg-[rgba(0,0,0,0.5)]" />
+        <p>or, {props.text} in with e-mail </p>
+        <hr className="flex h-[2px] w-[100px] items-end bg-[rgba(0,0,0,0.5)]" />
       </div>
     </>
   );
