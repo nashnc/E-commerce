977846692bef679b8643739d5535db09720d0e5a
nashnc <nash.nc@gmail.com>
2025-07-27 12:49:02 +0530

    Day 27 E-Commerce Self-Project — chore: Remove unused code and comments

--- adminPanel/src/Pages/Products/Products.jsx
+++ adminPanel/src/Pages/Products/Products.jsx
@@ -1,8 +1,10 @@
 import React, { useContext } from "react";
 import Header from "../../Components/Header/Header";
 import SideBar from "../../Components/SideBar/Sidebar";
-import { MyContext } from "../../App"; // Update this line
+import { MyContext } from "../../App";
 import ProductsHome from "../../Components/Products/ProductsHome";
+import ProductsHomeProductsTable from "../../Components/Products/ProductsHomeProductsTable";
+
 const Products = () => {
   const context = useContext(MyContext);
   return (
@@ -19,9 +21,10 @@ const Products = () => {
             className={`contentRight transition-all duration-[1s] ${context.sideBarIsOpen ? "w-[82%]" : "w-[90%]"} px-5 py-4`}
           >
             <ProductsHome />
+            <ProductsHomeProductsTable />
           </div>
         </div>
-      </section>z
+      </section>
     </>
   );
 };
