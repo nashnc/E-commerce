55644c93fdbb58f89499dc72aa5ea1d7184f8cf6
nashnc <nash.nc@gmail.com>
2025-07-27 12:57:51 +0530

    Day 27 E-commerce Self-Project: feat(sub-category): add MUI modal for managing sub-category list with AddCategoryList component

---
+++ adminPanel/src/Pages/Category/CategoryListUploading.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+  Dialog,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+  Slide,
+} from "@mui/material";
+import { IoClose } from "react-icons/io5";
+import AddCategoryList from "../../Components/Category/AddCategoryList";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const CategoryListUploading = ({ isOpen, onClose, type }) => {
+  return (
+    <div className="bg-[#f1f1f1]">
+      <Dialog
+        fullScreen
+        open={isOpen}
+        onClose={onClose}
+        TransitionComponent={Transition}
+      >
+        <AppBar sx={{ position: "relative" }}>
+          <Toolbar>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              Add Home Banners
+            </Typography>
+    
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={onClose}
+              aria-label="close"
+            >
+              <IoClose />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+
+        <div style={{ padding: "2rem" }}>
+          <>
+            <AddCategoryList type={type} />
+          </>
+        </div>
+      </Dialog>
+    </div>
+  );
+};
+
+export default CategoryListUploading;
