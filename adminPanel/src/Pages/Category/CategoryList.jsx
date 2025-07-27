7440d821f150e7e09ce17953a561ad655e2796f1
nashnc <nash.nc@gmail.com>
2025-07-27 12:58:55 +0530

    Day 27 E-commerce Self-Project: feat(category): create admin category display page with Header, Sidebar, CategoryListHome, and CategoryListTable components

---
+++ adminPanel/src/Pages/Category/CategoryList.jsx
@@ -0,0 +1,31 @@
+import React, { useContext } from "react";
+import Header from "../../Components/Header/Header";
+import SideBar from "../../Components/SideBar/Sidebar";
+import { MyContext } from "../../App";
+import CategoryListTable from "../../Components/Category/CategoryListTable";
+import CategoryListHome from "../../Components/Category/CategoryListHome";
+const CategoryList = () => {
+  const context = useContext(MyContext);
+  return (
+    <>
+      <section className="main">
+        <Header />
+        <div className="contentMain flex">
+          <div
+            className={`sideWrapper transition-all duration-[1s] ${context.sideBarIsOpen ? "w-[18%]" : "w-[4%] overflow-hidden"}`}
+          >
+            <SideBar />
+          </div>
+          <div
+            className={`contentRight transition-all duration-[1s] ${context.sideBarIsOpen ? "w-[82%]" : "w-[90%]"} px-5 py-4`}
+          >
+            <CategoryListHome />
+            <CategoryListTable />
+          </div>
+        </div>
+      </section>
+      z
+    </>
+  );
+};
+export default CategoryList;
