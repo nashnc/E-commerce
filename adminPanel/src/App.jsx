50f77915e1f6271cd00758b24d262935185cb5fe
nashnc <nash.nc@gmail.com>
2025-07-28 14:07:47 +0530

    day 28 e-commerce self-project :feat :added route to HomeBanners, Category List, SubCategoryList.Users.Orders

--- adminPanel/src/App.jsx
+++ adminPanel/src/App.jsx
@@ -6,6 +6,11 @@ import Navigations from "./Components/Navigations/Navigations";
 import Login from "./Pages/Login/Login";
 import Signup from "./Pages/Signup/Signup";
 import Products from "./Pages/Products/Products";
+import HomeBanners from "./Pages/HomeBanners/HomeBanners";
+import CategoryList from "./Pages/Category/CategoryList";
+import SubCategoryList from "./Pages/Category/SubCategoryList";
+import Users from "./Pages/Users/Users";
+import Orders from "./Pages/Orders/Orders";
 
 export const MyContext = createContext();
 
@@ -34,8 +39,32 @@ function App() {
       element: <Signup />,
     },
     {
-      path: "/product",
+      path: "/products",
       element: <Products />,
+    },
+    {
+      path: "/homeBanners/list",
+      element: <HomeBanners />,
+    },
+    {
+      path: "/homeBanners/list",
+      element: <HomeBanners />,
+    },
+    {
+      path: "/category/list",
+      element: <CategoryList />,
+    },
+    {
+      path: "/subcategory/list",
+      element: <SubCategoryList />,
+    },
+    {
+      path: "/users/list",
+      element: <Users />,
+    },
+    {
+      path: "/orders",
+      element: <Orders />,
     },
   ]);
 
