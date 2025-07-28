bea5eb850110f9ba751dfb92007cc5e766e9af02
nashnc <nash.nc@gmail.com>
2025-07-28 14:06:34 +0530

    day 28 e-commerce self-project : fix : written conditions  based  on props for tha badge to work  properly

--- adminPanel/src/Components/Badge/Badge.jsx
+++ adminPanel/src/Components/Badge/Badge.jsx
@@ -3,11 +3,24 @@ import React from "react";
 const Badge = (props) => {
   return (
     <>
-      <span
-        className={`flex items-center justify-center rounded-full px-2 py-1 text-[12px] capitalize ${props.status === "pending" && "bg-primary text-white"} ${props.status === "delivered" && "bg-green-700 text-white"} ${props.status === "confirm" && "bg-green-500 text-white"} `}
-      >
-        {props.status}
-      </span>
+      {props.titles === "" ? (
+        <p
+          className={` ${props.status === "delivered" && "bg-green-700 text-white"} ${props.status === "confirm" && "bg-green-500 text-white"} `}
+        >
+          {props.status}
+        </p>
+      ) : (
+        <>
+          {props.titles.map((title, index) => (
+            <p
+              key={index}
+              className="m-2 inline-block rounded-full bg-primary px-2 py-1 text-[14px] text-white"
+            >
+              {title}
+            </p>
+          ))}
+        </>
+      )}
     </>
   );
 };
