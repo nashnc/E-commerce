5f4f47af89027591c27abd82d26b9afb22da324b
nashnc <nash.nc@gmail.com>
2025-07-30 16:52:17 +0530

    day 30 e-commerce self-project : feat : created a module to connect to the DB in mongo atlas

---
+++ serverSide/config/connectDB.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+if (!process.env.MONGO_URI) {
+  throw new Error("MONGO_URI is not defined in .env");
+}
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
+}
+
+export default connectDB;
